3689c4520263cd668d79f6ecb938349bb450a6b2
Ilya Mafov <meat.100@gmail.com>
2020-01-24 13:35:46 +0300

    Изменение структуры каталогов

---
+++ src/ru/geekbrains/lessonfour/GameMult.java
@@ -0,0 +1,61 @@
+package ru.geekbrains.lessonfour;
+
+import java.util.Random;
+
+public class GameMult {
+    private static final char DOT_X = 'X';
+    private static final char DOT_O = 'O';
+    private static int turnCount = 0;
+    private static Random random = new Random();
+    private static Map map = new Map();
+
+
+    private static void humanTurn(char dot) {
+        int x, y;
+        do {
+            System.out.println("Ход игрока " + dot);
+            System.out.println("Введите координаты в формате X Y.\nОт 0 до " + Map.SIZE);
+            while(!Main.scanner.hasNextInt()) {
+                System.out.println("Введите число!");
+                Main.scanner.next();
+            }
+            x = Main.scanner.nextInt() - 1;
+            while(!Main.scanner.hasNextInt()) {
+                System.out.println("Введите число!");
+                Main.scanner.next();
+            }
+            y = Main.scanner.nextInt() - 1;
+        } while (map.isCellValid(x, y));
+        map.setMap(x, y, dot);
+        turnCount++;
+    }
+
+    private static boolean endTurn(char dot) {
+        if(map.checkWin(dot)) {
+            System.out.println("Победил игрок " + dot);
+            return true;
+        }
+        if (map.isMapFull()) {
+            System.out.println("Ничья");
+            return true;
+        }
+        return false;
+    }
+
+    public static void startGame(){
+        map.getMap();
+        while(true){
+            humanTurn(DOT_X);
+            map.getMap();
+            if(turnCount >= 4) {
+                if(endTurn(DOT_X)) break;
+            }
+            humanTurn(DOT_O);
+            map.getMap();
+            if(turnCount >= 4) {
+                if(endTurn(DOT_O)) break;
+            }
+        }
+    }
+
+}
---
+++ src/ru/geekbrains/lessonfour/Map.java
@@ -0,0 +1,102 @@
+package ru.geekbrains.lessonfour;
+
+public class Map {
+    private static final char DOT_EMPTY = '•';
+    public static int SIZE = 3;
+    private static char[][] map;
+
+    public Map() {
+        map = new char[SIZE][SIZE];
+        for (int i = 0; i < SIZE; i++) {
+            for (int j = 0; j < SIZE; j++) {
+                map[i][j] = DOT_EMPTY;
+            }
+        }
+    }
+
+    public void getMap() {
+        for (int i = 0; i <= SIZE; i++) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+        for (int i = 0; i < SIZE; i++) {
+            System.out.print((i + 1) + " ");
+            for (int j = 0; j < SIZE; j++) {
+                System.out.print(map[i][j] + " ");
+            }
+            System.out.println();
+        }
+        System.out.println();
+    }
+
+    public void setMap(int x, int y, char dot) {
+        map[y][x] = dot;
+    }
+
+    public boolean isCellValid(int x, int y) {
+        if (x < 0 || x >= SIZE || y < 0 || y >= SIZE) return true;
+        return map[y][x] != DOT_EMPTY;
+    }
+
+    public boolean checkWin(char symbol) {
+        for (int i = 0; i < SIZE; i++)
+            if ((map[i][0] == symbol && map[i][1] == symbol && map[i][2] == symbol) ||
+                    (map[0][i] == symbol && map[1][i] == symbol && map[2][i] == symbol))
+                return true;
+            return (map[0][0] == symbol && map[1][1] == symbol && map[2][2] == symbol) ||
+                (map[2][0] == symbol && map[1][1] == symbol && map[0][2] == symbol);
+    }
+    //Методы getAiX и getAiY получают координату, чтобы перекрыть победный ход игроку (задание 4)
+    public int getAiX(char symbolHum, char symbolAi) {
+        for (int i = 0; i < SIZE; i++) {
+            if (map[i][1] == symbolHum && map[i][2] == symbolHum && map[i][0] != symbolAi) return 0;
+            else if (map[i][0] == symbolHum && map[i][2] == symbolHum && map[i][1] != symbolAi) return 1;
+            else if (map[i][0] == symbolHum && map[i][1] == symbolHum && map[i][2] != symbolAi) return 2;
+
+            else if (map[1][i] == symbolHum && map[2][i] == symbolHum && map[0][i] != symbolAi) return i;
+            else if (map[0][i] == symbolHum && map[2][i] == symbolHum && map[1][i] != symbolAi) return i;
+            else if (map[0][i] == symbolHum && map[1][i] == symbolHum && map[2][i] != symbolAi) return i;
+        }
+        if (map[1][1] == symbolHum && map[2][2] == symbolHum && map[0][0] != symbolAi) return 0;
+        else if (map[0][0] == symbolHum && map[2][2] == symbolHum && map[1][1] != symbolAi) return 1;
+        else if (map[0][0] == symbolHum && map[1][1] == symbolHum && map[2][2] != symbolAi) return 2;
+
+        else if (map[0][2] == symbolHum && map[1][1] == symbolHum  && map[2][0] != symbolAi) return 0;
+        else if (map[0][2] == symbolHum && map[2][0] == symbolHum  && map[1][1] != symbolAi) return 1;
+        else if (map[1][1] == symbolHum && map[2][0] == symbolHum  && map[0][2] != symbolAi) return 2;
+        return 3;
+    }
+
+    public int getAiY(char symbolHum, char symbolAi) {
+        for (int i = 0; i < SIZE; i++){
+            if (map[i][1] == symbolHum && map[i][2] == symbolHum && map[i][0] != symbolAi) return i;
+            else if (map[i][0] == symbolHum && map[i][2] == symbolHum && map[i][1] != symbolAi) return i;
+            else if (map[i][0] == symbolHum && map[i][1] == symbolHum && map[i][2] != symbolAi) return i;
+
+            else if (map[1][i] == symbolHum && map[2][i] == symbolHum && map[0][i] != symbolAi) return 0;
+            else if (map[0][i] == symbolHum && map[2][i] == symbolHum && map[1][i] != symbolAi) return 1;
+            else if (map[0][i] == symbolHum && map[1][i] == symbolHum && map[2][i] != symbolAi) return 2;
+        }
+        if (map[1][1] == symbolHum && map[2][2] == symbolHum && map[0][0] != symbolAi) return 0;
+        else if (map[0][0] == symbolHum && map[2][2] == symbolHum && map[1][1] != symbolAi) return 1;
+        else if (map[0][0] == symbolHum && map[1][1] == symbolHum && map[2][2] != symbolAi) return 2;
+
+        else if (map[0][2] == symbolHum && map[1][1] == symbolHum  && map[2][0] != symbolAi) return 2;
+        else if (map[0][2] == symbolHum && map[2][0] == symbolHum  && map[1][1] != symbolAi) return 1;
+        else if (map[1][1] == symbolHum && map[2][0] == symbolHum  && map[0][2] != symbolAi) return 0;
+        return 3;
+    }
+
+    public boolean isMapFull() {
+        for (int i = 0; i < SIZE; i++) {
+            for (int j = 0; j < SIZE; j++) {
+                if (map[i][j] == DOT_EMPTY) return false;
+            }
+        }
+        return true;
+    }
+
+
+
+
+}

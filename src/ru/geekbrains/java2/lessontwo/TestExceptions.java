3c3bb8966f8bbc1ae8a0ff8d0cbfa143e360898a
meat08 <42217126+meat08@users.noreply.github.com>
2020-03-03 12:55:46 +0300

    Update TestExceptions.java

--- src/ru/geekbrains/java2/lessontwo/TestExceptions.java
+++ src/ru/geekbrains/java2/lessontwo/TestExceptions.java
@@ -20,9 +20,9 @@ public class TestExceptions {
     }
 
     public static int myExceptions(String[][] arr) throws MyArraySizeException, MyArrayDataException {
-        if (arr.length > 4) throw new MyArraySizeException();
+        if (arr.length != 4) throw new MyArraySizeException();
         for(int i = 0; i < arr.length; i++) {
-            if(arr[i].length > 4) throw new MyArraySizeException();
+            if(arr[i].length != 4) throw new MyArraySizeException();
         }
         int sum = 0;
         for (int x = 0; x < arr.length; x++) {

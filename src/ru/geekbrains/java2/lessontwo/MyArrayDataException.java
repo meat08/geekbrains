a967ede65b548b2cc8f1b6d02ebceb9d33945fdb
meat08 <42217126+meat08@users.noreply.github.com>
2020-03-03 12:51:48 +0300

    Update MyArrayDataException.java

--- src/ru/geekbrains/java2/lessontwo/MyArrayDataException.java
+++ src/ru/geekbrains/java2/lessontwo/MyArrayDataException.java
@@ -1,16 +1,7 @@
 package ru.geekbrains.java2.lessontwo;
 
 public class MyArrayDataException extends  NumberFormatException {
-    private int x;
-    private int y;
-
     public MyArrayDataException (int x, int y) {
-        this.x = x;
-        this.y = y;
-    }
-
-    @Override
-    public String getMessage() {
-        return "Ошибка преобразования. В ячейке [" + x + "][" + y + "] не число.";
+        super("Ошибка преобразования. В ячейке [" + x + "][" + y + "] не число.");
     }
 }

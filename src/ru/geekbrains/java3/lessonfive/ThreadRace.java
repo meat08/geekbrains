1d99c82db59301d3477960e2530aee752fa03008
meat08 <42217126+meat08@users.noreply.github.com>
2020-04-09 20:43:18 +0300

    J3 ДЗ Урок 5 (#21)
    * J3 ДЗ Урок 5

---
+++ src/ru/geekbrains/java3/lessonfive/ThreadRace.java
@@ -0,0 +1,29 @@
+package ru.geekbrains.java3.lessonfive;
+
+import java.util.concurrent.CountDownLatch;
+
+public class ThreadRace {
+    public static final int CARS_COUNT = 4;
+
+    public static void main(String[] args) {
+        System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Подготовка!!!");
+        Race race = new Race(new Road(60), new Tunnel(CARS_COUNT), new Road(40));
+        CountDownLatch latchCars = new CountDownLatch(CARS_COUNT);
+        CountDownLatch latchRaces = new CountDownLatch(race.getStages().size() * CARS_COUNT);
+        Car[] cars = new Car[CARS_COUNT];
+        for (int i = 0; i < cars.length; i++) {
+            cars[i] = new Car(latchCars, latchRaces, race, 20 + (int) (Math.random() * 10));
+        }
+        for (Car car : cars) {
+            new Thread(car).start();
+        }
+        try {
+            latchCars.await();
+            System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Гонка началась!!!");
+            latchRaces.await();
+            System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Гонка закончилась!!!");
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}

335cadedc4452a3e3a86b4caa5edbfd800f4ad11
Ilya Mafov <meat.100@gmail.com>
2020-04-15 13:47:34 +0300

    J3 ДЗ Урок 7

---
+++ src/main/java/ru/geekbrains/java3/lessonseven/RunMyTests.java
@@ -0,0 +1,62 @@
+package ru.geekbrains.java3.lessonseven;
+
+import ru.geekbrains.java3.lessonseven.annotations.AfterSuite;
+import ru.geekbrains.java3.lessonseven.annotations.BeforeSuite;
+import ru.geekbrains.java3.lessonseven.annotations.Test;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.*;
+
+public class RunMyTests {
+
+    private static final int MIN_PRIORITY = 1;
+    private static final int MAX_PRIORITY = 10;
+    private static int beforeAnnotationCount = 0;
+    private static int afterAnnotationCount = 0;
+    private static final Map<Integer, Set<Method>> priorityMap = new TreeMap<>();
+
+    private static void add(int priority, Method method) {
+        Set<Method> methodsSet = getPriority(priority);
+        methodsSet.add(method);
+    }
+
+    private static Set<Method> getPriority(int priority) {
+        return priorityMap.computeIfAbsent(priority, key -> new HashSet<>());
+    }
+
+    public static void start(Class<?> className) {
+
+        for (Method method :className.getDeclaredMethods()) {
+            if (method.getAnnotation(BeforeSuite.class) != null) {
+                add(MIN_PRIORITY - 1, method);
+                beforeAnnotationCount++;
+                if(beforeAnnotationCount > 1) throw new RuntimeException("@BeforeSuite больше 1");
+            }
+            if (method.getAnnotation(AfterSuite.class) != null) {
+                add(MAX_PRIORITY + 1, method);
+                afterAnnotationCount++;
+                if(afterAnnotationCount > 1) throw new RuntimeException("@AfterSuite больше 1");
+            }
+            if (method.getAnnotation(Test.class) != null) {
+                Test test = method.getAnnotation(Test.class);
+                add(test.priority(), method);
+            }
+        }
+
+        try {
+            Object testClass = className.newInstance();
+            for (Integer key : priorityMap.keySet()) {
+                for (Method method : priorityMap.get(key)) {
+                    method.invoke(testClass);
+                }
+            }
+        } catch (InstantiationException | IllegalAccessException | InvocationTargetException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        start(TestClass.class);
+    }
+}

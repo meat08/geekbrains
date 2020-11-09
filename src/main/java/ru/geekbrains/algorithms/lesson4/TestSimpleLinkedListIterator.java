21d738371ea494b81d30d6e2971ba71660c173f4
meat08 <42217126+meat08@users.noreply.github.com>
2020-11-09 12:02:35 +0300

    HW lesson 4 (#28)
    * HW lesson 4

---
+++ src/main/java/ru/geekbrains/algorithms/lesson4/TestSimpleLinkedListIterator.java
@@ -0,0 +1,21 @@
+package ru.geekbrains.algorithms.lesson4;
+
+import ru.geekbrains.algorithms.base.mylinkedlist.LinkedList;
+import ru.geekbrains.algorithms.base.mylinkedlist.SimpleLinkedListImpl;
+
+public class TestSimpleLinkedListIterator {
+    public static void main(String[] args) {
+        LinkedList<Integer> list = new SimpleLinkedListImpl<>();
+        list.insertFirst(1);
+        list.insertFirst(2);
+        list.insertFirst(3);
+        list.insertFirst(4);
+        list.insertFirst(5);
+        list.display();
+
+        for (Integer i : list) {
+            System.out.println(i);
+        }
+
+    }
+}

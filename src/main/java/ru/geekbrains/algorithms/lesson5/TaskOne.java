fcc7beb16d4a844cbaa6c2dea5ee1d9788f85bd6
Ilya Mafov <meat.100@gmail.com>
2020-11-12 21:25:45 +0300

    Reworked task2 for negative numbers

--- src/main/java/ru/geekbrains/algorithms/lesson5/TaskOne.java
+++ src/main/java/ru/geekbrains/algorithms/lesson5/TaskOne.java
@@ -2,13 +2,24 @@ package ru.geekbrains.algorithms.lesson5;
 
 public class TaskOne {
     public static void main(String[] args) {
-        System.out.println(exponentiation(2, 2));
+        System.out.println(exponentiation(2, -3));
+        System.out.println(exponentiation(3, 3));
+        System.out.println(exponentiation(8, 6));
+        System.out.println(exponentiation(128, 0));
     }
 
-    public static int exponentiation(int n, int degree) {
-        if (degree == 1) {
-            return n;
+    public static double exponentiation(double n, int degree) {
+        if (n == 0) {
+            throw new IllegalArgumentException("n cannot be 0");
         }
-        return n * exponentiation(n, degree - 1);
+        if (degree > 0) {
+            return n * exponentiation(n, degree - 1);
+        }
+        if (degree < 0) {
+            return 1 / (n * exponentiation(n, -degree - 1));
+        }
+        else {
+            return 1;
+        }
     }
 }

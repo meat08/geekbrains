0f6e32c120601a932c22fb6c9a24a48094652f78
Ilya Mafov <meat.100@gmail.com>
2020-11-13 14:18:19 +0300

    Prepare project

---
+++ src/main/java/ru/geekbrains/algorithms/base/mytree/Tree.java
@@ -0,0 +1,19 @@
+package ru.geekbrains.algorithms.base.mytree;
+
+import java.util.function.Consumer;
+
+public interface Tree<E extends Comparable<? super E>> {
+    enum TraverseMode {
+        IN_ORDER,
+        PRE_ORDER,
+        POST_ORDER
+    }
+
+    boolean add(E value);
+    boolean remove(E value);
+    boolean contains(E value);
+    boolean isEmpty();
+    int size();
+    void display();
+    void traverse(TraverseMode mode, Consumer<E> action);
+}

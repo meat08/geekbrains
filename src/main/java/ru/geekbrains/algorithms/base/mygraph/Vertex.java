8b4e37ea3a125fef7dee013a6e2e1d9e4bde35f0
Ilya Mafov <meat.100@gmail.com>
2020-11-17 12:29:39 +0300

    Prepare project

---
+++ src/main/java/ru/geekbrains/algorithms/base/mygraph/Vertex.java
@@ -0,0 +1,38 @@
+package ru.geekbrains.algorithms.base.mygraph;
+
+import java.util.Objects;
+
+public class Vertex {
+
+    private final String label;
+    private boolean visited;
+
+    public Vertex(String label) {
+        this.label = label;
+    }
+
+    public String getLabel() {
+        return label;
+    }
+
+    public boolean isVisited() {
+        return visited;
+    }
+
+    public void setVisited(boolean visited) {
+        this.visited = visited;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(label);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+        Vertex vertex = (Vertex) obj;
+        return Objects.equals(label, vertex.label);
+    }
+}

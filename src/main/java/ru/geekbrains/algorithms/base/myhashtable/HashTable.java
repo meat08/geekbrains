1265436c390a4254a40150b2b133ea0d02406865
Ilya Mafov <meat.100@gmail.com>
2020-11-20 13:59:58 +0300

    Prepare project

---
+++ src/main/java/ru/geekbrains/algorithms/base/myhashtable/HashTable.java
@@ -0,0 +1,16 @@
+package ru.geekbrains.algorithms.base.myhashtable;
+
+public interface HashTable<K, V> {
+    boolean put(K key, V value);
+    V get(K key);
+    V remove(K key);
+    int size();
+    boolean isEmpty();
+    void display();
+
+    interface Entry<K, V> {
+        K getKey();
+        V getValue();
+        void setValue(V value);
+    }
+}

b72fbebbbc22d14435a320dad643372918030889
Ilya Mafov <meat.100@gmail.com>
2021-01-13 15:03:37 +0300

    HW lesson 5

--- src/main/java/ru/geekbrains/algorithms/base/mylinkedlist/LinkedList.java
+++ src/main/java/ru/geekbrains/algorithms/base/mylinkedlist/LinkedList.java
@@ -1,7 +1,8 @@
 package ru.geekbrains.algorithms.base.mylinkedlist;
 
+import java.util.Iterator;
 
-public interface LinkedList<E> {
+public interface LinkedList<E> extends Iterable<E> {
 
     void insertFirst(E value);
     E removeFirst();
@@ -11,6 +12,7 @@ public interface LinkedList<E> {
     int size();
     boolean isEmpty();
     void display();
+    Iterator<E> iterator();
 
     class Node<E> {
         E item;
@@ -19,6 +21,27 @@ public interface LinkedList<E> {
         public Node(E item, Node<E> next) {
             this.item = item;
             this.next = next;
+        }
+    }
+
+    class ListIterator<E> implements Iterator<E> {
+
+        private Node<E> current;
+
+        public ListIterator(Node<E> first) {
+            this.current = first;
+        }
+
+        @Override
+        public boolean hasNext() {
+            return current != null;
+        }
+
+        @Override
+        public E next() {
+            E temp = current.item;
+            current = current.next;
+            return temp;
         }
     }
 }
